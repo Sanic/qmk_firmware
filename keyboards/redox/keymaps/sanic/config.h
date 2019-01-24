76da0145190d56ebbec66f807de3eb34f394c922
pmania <Sanic>
2019-01-24 20:47:28 +0100

    implemented color switch per layer; reduced TAP_CODE_DELAY to 10

--- keyboards/redox/keymaps/sanic/config.h
+++ keyboards/redox/keymaps/sanic/config.h
@@ -31,3 +31,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 
 #define TAPPING_TERM 200
+#define TAP_CODE_DELAY 10
--- keyboards/redox/keymaps/sanic/keymap.c
+++ keyboards/redox/keymaps/sanic/keymap.c
@@ -32,6 +32,8 @@
 
 extern keymap_config_t keymap_config;
 
+
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -48,6 +50,75 @@ enum custom_keycodes {
   ADJUST,
 };
 
+bool has_layer_changed = false;
+static uint8_t current_layer;
+static uint8_t previous_layer;
+
+extern rgblight_config_t rgblight_config;
+
+void matrix_scan_user(void){
+  uint8_t layer = biton32(layer_state);
+
+  if(current_layer == layer){
+    has_layer_changed = false;
+  }else
+  {
+    has_layer_changed = true;
+    previous_layer = current_layer;
+    current_layer = layer;
+  }
+
+  if(has_layer_changed)
+  {
+    /* uint8_t rgbmode = rgblight_get_mode(); */
+    rgblight_config.raw = eeconfig_read_rgblight();
+
+    switch(current_layer) {
+      case _QWERTY:
+
+        if(!rgblight_config.enable){
+          // If rgb is supposed to be off (no manual config), disable it
+          rgblight_disable();
+        }else{
+          // Else, restore from eeprom
+          rgblight_mode_noeeprom(rgblight_config.mode);
+
+        }
+        /* if(rgbmode == RGBLIGHT_MODE_STATIC_LIGHT) */
+
+
+    /*   // TODO load current rgb light eeprom stuff if coming from adjust layer? */
+      break;
+      case _SYMB:
+        rgblight_enable_noeeprom();
+        rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+        rgblight_sethsv_noeeprom_red();
+        /* rgblight_enable(); */
+        /* rgblight_setrgb(255,0,0); */
+      break;
+      case _NAV:
+        rgblight_enable_noeeprom();
+        rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+        rgblight_sethsv_noeeprom_green();
+        /* rgblight_enable(); */
+        /* rgblight_setrgb(0,255,0); */
+      break;
+      case _ADJUST:
+        rgblight_enable_noeeprom();
+        rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+        rgblight_sethsv_noeeprom_blue();
+        /* rgblight_enable(); */
+        /* rgblight_setrgb(0,0,255); */
+      break;
+    }
+    /* if(current_layer == _QWERTY){ */
+    /*   rgblight_disable(); */
+    /* }else{ */
+    /*   rgblight_enable(); */
+    /* } */
+  }
+}
+
 // Shortcut to make keymap more readable
 #define KC_BKSL KC_BSLASH
 #define SYM_L   MO(_SYMB)
@@ -55,8 +126,9 @@ enum custom_keycodes {
 #define KC_SYQU   LT(_SYMB, KC_QUOTE)
 
 
-#define KC_ALAS LALT_T(KC_PAST)
-#define KC_CTPL LCTL_T(KC_PSLS)
+#define KC_ALDO LALT_T(KC_DOWN)
+/* #define KC_ALAS LALT_T(KC_PAST) */
+/* #define KC_CTPL LCTL_T(KC_PSLS) */
 
 // #define KC_NAGR LT(_NAV, KC_GRV)
 
@@ -64,9 +136,11 @@ enum custom_keycodes {
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
 #define KC_ADES LT(_ADJUST, KC_ESC)
 
-#define KC_NAEN LT(_NAV, KC_ENTER)
+#define KC_NAES LT(_NAV, KC_ESC)
+#define KC_NAF LT(_NAV, KC_F)
 
 #define KC_LAUP LALT_T(KC_UP)
+#define KC_LALE LALT_T(KC_LEFT)
 
 #define KC_GUBS LGUI_T(KC_BSPACE)
 #define KC_CTTA LCTL_T(KC_TAB)
@@ -88,11 +162,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,TSYM_L  ,                          TSYM_L  ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BKSL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NAEN ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_HYPR ,                          KC_MEH  ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_SYQU ,
+     KC_NAES ,KC_A    ,KC_S    ,KC_D    ,KC_NAF  ,KC_G    ,KC_HYPR ,                          KC_MEH  ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_SYQU ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSPO ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADES ,_______ ,        _______ ,KC_ADES ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSPC ,
+     KC_LSPO ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADES ,KC_HOME ,        KC_END  ,KC_ADES ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSPC ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     _______ ,_______ ,_______ ,KC_DOWN ,     KC_LAUP ,    KC_GUBS ,KC_CTTA ,        KC_CTEN ,KC_GUSP ,    KC_LEFT ,     KC_RIGHT,KC_LBRC ,KC_RBRC ,SYM_L 
+     _______ ,_______ ,_______ ,KC_DOWN ,     KC_LAUP ,    KC_GUBS ,KC_CTTA ,        KC_CTEN ,KC_GUSP ,    KC_LALE ,     KC_RIGHT,KC_LBRC ,KC_RBRC ,SYM_L 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -116,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,KC_WH_U ,KC_MS_U ,KC_WH_D ,XXXXXXX ,_______ ,                          _______ ,XXXXXXX ,KC_LCTA ,KC_UP   ,KC_LCTE ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,KC_F11  ,KC_MS_L ,KC_MS_D ,KC_MS_R ,XXXXXXX ,_______ ,                          _______ ,_______ ,KC_LEFT ,KC_DOWN ,KC_RIGHT,XXXXXXX ,XXXXXXX ,
+     _______ ,KC_F11  ,KC_MS_L ,KC_MS_D ,_______ ,XXXXXXX ,_______ ,                          _______ ,_______ ,KC_LEFT ,KC_DOWN ,KC_RIGHT,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,_______ ,        _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
@@ -128,11 +202,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      XXXXXXX ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,RESET   ,RGB_M_P ,RGB_TOG ,RGB_MOD ,RGB_HUD ,RGB_HUI ,                          RGB_SAD ,RGB_SAI ,RGB_VAD ,RGB_VAI ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+     XXXXXXX ,XXXXXXX ,RGB_M_P ,RGB_TOG ,RGB_MOD ,RGB_HUD ,RGB_HUI ,                          RGB_SAD ,RGB_SAI ,RGB_VAD ,RGB_VAI ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,XXXXXXX ,        XXXXXXX ,_______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+     XXXXXXX ,RESET   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,XXXXXXX ,        XXXXXXX ,_______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
